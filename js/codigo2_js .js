dc5c2ac2da6c6a2f827dd76ffc56d6881a44abac
oscarmanuelsanchez <oscarsanchez@colegioguanenta.edu.co>
2022-05-09 15:43:53 -0500

    Trabajo 2

--- js/codigo2_js .js
+++ js/codigo2_js .js
@@ -1,20 +1,31 @@
-function numeroMayor()
+function diasemana()
 {
-    numero1 = parseInt(document.formulario.caja1.value);
-    numero2 = parseInt(document.formulario.caja2.value);
-    numero3 = parseInt(document.formulario.caja3.value);
+    dia = parseInt(document.formulario.caja.value);
+    switch(dia)
+    {
+        case 1:
+            document.formulario.respuesta.value = "Lunes"
+            break;
+        case 2:
+            document.formulario.respuesta.value = "Martes"
+            break;
+        case 3:
+            document.formulario.respuesta.value = "Miercoles"
+            break;
+        case 4:
+            document.formulario.respuesta.value = "Jueves"
+            break;
+        case 5:
+            document.formulario.respuesta.value = "Viernes"
+            break;
+        case 6:
+            document.formulario.respuesta.value = "Sabado"
+            break;
+        case 7:
+            document.formulario.respuesta.value = "Domingo"
+            break;
+        default:
+            document.formulario.respuesta.value = "No es un dia de la semana"
 
-    if(numero1>numero2 && numero1>numero3)
-    {
-        mayor = numero1;
     }
-    if(numero2>numero1 && numero2>numero3)
-    {
-        mayor = numero2;
-    }
-    if(numero3>numero1 && numero3>numero2)
-    {
-        mayor = numero3;
-    }
-    document.formulario.caja4.value = mayor;
 }
